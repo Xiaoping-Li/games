ba8bba6cd74d947af8ce7d3c98910bd8f63fb08c
Xiaoping Li <narci2008@gmail.com>
2019-02-09 14:50:41 -0800

    Add board style

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -5,13 +5,33 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: ['', '', '', '', '', '', '', '', ''],
+      data: ['', 'x', 'o', '', 'x', 'o', 'o', 'x', 'x'],
     };
   }
   render() {
     return (
       <div className="App">
         <h1>Tic-Tac-Toe</h1>
+
+        {/* Board */}
+        <table className="board">
+          <tr>
+            <td>{this.state.data[0]}</td>
+            <td className="middle-column">{this.state.data[1]}</td>
+            <td>{this.state.data[2]}</td>
+          </tr>
+          <tr >
+            <td className="middle-row">{this.state.data[3]}</td>
+            <td className="middle-row middle-column">{this.state.data[4]}</td>
+            <td className="middle-row">{this.state.data[5]}</td>
+          </tr>
+          <tr>
+            <td>{this.state.data[6]}</td>
+            <td className="middle-column">{this.state.data[7]}</td>
+            <td>{this.state.data[8]}</td>
+          </tr>
+        </table>
+        
       </div>
     );
   }
