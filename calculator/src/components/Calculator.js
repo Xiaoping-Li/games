3f4e96fc2c1d030276ba9853547f232b4cce1ac3
Xiaoping-Li <narci2008@gmail.com>
2019-02-19 16:52:07 -0800

    Add click function

--- calculator/src/components/Calculator.js
+++ calculator/src/components/Calculator.js
@@ -2,18 +2,49 @@ import React, { Component } from 'react';
 import '../CSS/calculator.css';
 
 class Calculator extends Component {
-  render() {
-    return (
-      <div className="calculator">
-        <form>
-            <input type="text" />
-        </form>    
-        <div className="buttons">
-            {keys.map((key, idx) => <button className="grid-item" key={idx}>{key}</button>)}
-        </div>  
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            result: 0,
+        };
+    }
+
+    handleClick = (e) => {
+        const btnValue = e.target.value;
+        if (btnValue === '=') {
+            console.log('calculating...');
+            return;
+        } else if (btnValue === 'AC') {
+            console.log('deleting...');
+            return;
+        } else {
+            const updateInput = this.state.input + btnValue;
+            this.setState({input: updateInput});
+        }
+    }
+
+    render() {
+        return (
+            <div className="calculator">
+                <div className="screen">
+                    {this.state.input}
+                </div>    
+                <div className="buttons">
+                    {keys.map((key, idx) => 
+                        <button 
+                            className="grid-item"
+                            value={key} 
+                            onClick={this.handleClick}
+                            key={idx}
+                        >
+                            {key}
+                        </button>
+                    )}
+                </div>  
+            </div>
+        );
+    }
 }
 
 export default Calculator;
